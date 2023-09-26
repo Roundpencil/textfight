1c3a86f0769e97434af71205b1a78d3f498be2fa
work <pi-work@ssji.net>
2023-09-26 18:47:36 +0200

    iniialisation

--- main.py
+++ main.py
@@ -5,43 +5,66 @@ from dataclasses import dataclass
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-@dataclass
-class Contender:
-    description: str
-    wins = 0
-    loses = 0
+class tournoi:
+    def __init__(self, txt):
+        self.contenders = self._generate_contenders(txt)
+
+    @dataclass
+    class Contender:
+        description: str
+        wins = 0
+        loses = 0
 
 
-def generate_contenders(file_name: str) -> list[Contender]:
-    contenders = []
-    with open(file_name, 'r') as file:
-        for line in file:
-            if line.strip():  # skip empty lines
-                if line.strip()[0].isdigit():  # check if line starts with a digit
+
+    def _generate_contenders(self, file_name: str) -> list[Contender]:
+        to_return = []
+        with open(file_name, 'r') as file:
+            for line in file:
+                if line.strip() and line.strip()[0].isdigit():
                     description = line.split(".")[1].strip()
-                    contender = Contender(description)
-                    contenders.append(contender)
-    return contenders
+                    contender = self.Contender(description)
+                    to_return.append(contender)
+        return to_return
 
-def creer_appairements(contenders: list[Contender]):
-    to_return = []
-    haut = [contender for contender in contenders if contender.loses == 0]
-    moyen = [contender for contender in contenders if contender.loses == 1]
-    bas = [contender for contender in contenders if contender.loses == 2]
+    def creer_appaieiments(self, contenders: list[Contender]):
+        to_return = []
+        haut = [contender for contender in contenders if contender.loses == 0]
+        moyen = [contender for contender in contenders if contender.loses == 1]
+        bas = [contender for contender in contenders if contender.loses == 2]
 
-    for tableau in [haut, moyen, bas]:
-        if len(tableau) % 2 == 1:
-            tableau.append(None)
-        while len(tableau):
-            to_return.append([tableau.pop(), tableau.pop()])
+        for tableau in [haut, moyen, bas]:
+            if len(tableau) % 2 == 1:
+                tableau.append(None)
+            while len(tableau):
+                to_return.append([tableau.pop(), tableau.pop()])
+
+        return to_return
+
+    def ronde_en_cours(self, table_appariements):
+        if len(table_appariements):
+            return table_appariements[0][0].wins + table_appariements[0][0].loses
+
+        return -1
+
+    # Press the green button in the gutter to run the script.
+    def demander_vote_a_utilisateur(self, contender_1, contender_2):
+        pass
 
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    #lancement du programme : demander si fichier de sauvegarde ou nouveau projet (saisie)
+
     # Test with an example file name.
-    contenders = generate_contenders("source.txt")
+    contenders = tournoi("source.txt")
     for contender in contenders:
         print(contender)
+
+    appariements = creer_appaieiments(contenders)
+
+    while len(appariements):
+        contender_1, contender_2 = appariements.pop()
+        demander_vote_a_utilisateur(contender_1, contender_2)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
